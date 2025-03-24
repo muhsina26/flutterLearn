7895049aef1b69bd88dc91b7757abac70673eca4
muhsina26 <tasnim15-5763@diu.edu.bd>
2025-03-24 10:03:28 +0600

    textButtonDesign

--- lib/pages/currency_converter_material_page.dart
+++ lib/pages/currency_converter_material_page.dart
@@ -1,48 +1,75 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 class CurrencyConverterMaterialPage extends StatelessWidget {
   const CurrencyConverterMaterialPage({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    final border = OutlineInputBorder(
+      borderSide: const BorderSide(
+        width: 2.0,
+        style: BorderStyle.solid,
+      ),
+      borderRadius: BorderRadius.circular(5),
+    );
+
+    return Scaffold(
       backgroundColor: Colors.blueGrey,
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text(
+            const Text(
               '0',
               style: TextStyle(
-                  fontSize: 50,
-                  fontWeight: FontWeight.bold,
-                  color: Color.fromARGB(255, 0, 0, 0)
-                  ),
+                fontSize: 50,
+                fontWeight: FontWeight.bold,
+                color: Color.fromARGB(255, 0, 0, 0),
+              ),
             ),
-            TextField(
-                style: TextStyle(
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextField(
+                style: const TextStyle(
                   color: Color.fromARGB(255, 195, 37, 37),
                 ),
                 decoration: InputDecoration(
-                 hintText: "Please enter the amount in INR",
-                 hintStyle: TextStyle(
-                  color: Color.fromARGB(255, 8, 8, 8)
-                 ),
-                 suffixIcon:Icon(Icons.monetization_on_outlined),
-                 suffixIconColor: Colors.white,
-                 focusedBorder:OutlineInputBorder(
-                  borderSide: BorderSide(
-                    color: Colors.red,
-                    width: 2.0,
-                    style: BorderStyle.solid,
-                    strokeAlign: BorderSide.strokeAlignCenter
-                  )
-                 ),
-                 filled: true,
-                 fillColor:Colors.amberAccent
-
-                  
+                  hintText: "Please enter the amount in INR",
+                  hintStyle: const TextStyle(
+                    color: Color.fromARGB(255, 8, 8, 8),
                   ),
+                  prefixIcon: const Icon(Icons.monetization_on_outlined),
+                  filled: true,
+                  fillColor: Colors.white,
+                  focusedBorder: border,
+                  enabledBorder: border,
                 ),
+                keyboardType: const TextInputType.numberWithOptions(
+                  decimal: true,
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextButton(
+                onPressed: () {
+                  if (kDebugMode) {
+                    print("Button Clicked");
+                  }
+                },
+                style:TextButton.styleFrom(
+                   backgroundColor: Colors.black,
+                foregroundColor: Colors.white,
+                minimumSize: Size(double.infinity, 50),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(5),
+                ),
+                
+                ),
+                child: const Text("Convert"), // Fixed missing `const`
+              ),
+            ),
           ],
         ),
       ),
